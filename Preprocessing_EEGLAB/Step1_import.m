18c1f74c1fba940b085163b05772d064af15b101
Ettore Napoli <ettore.napoli@rc.fhsc.net@wmalaspina.rc.fhsc.net>
2026-03-12 10:52:22 +0100

    Modified input file

--- Preprocessing_EEGLAB/Step1_import.m
+++ Preprocessing_EEGLAB/Step1_import.m
@@ -2,11 +2,12 @@
 
 % Initialize EEGLAB
 addpath('/mnt/raid/software/eeglab2024.1/')
+
 eeglab
 
 % Set relevant folders
-input_folder = '/mnt/raid/RU1/Raw_data/Ettore/Studio_Rachi/Parkinson/Raw_Data/PARKINSON_OFF/'
-output_folder = '/mnt/raid/RU1/Raw_data/Ettore/Studio_Rachi/Parkinson/Preprocessing_Output/PD_OFF/Step1_Import'
+input_folder = '/mnt/raid/RU1/Raw_data/Ettore/Entropy/Healthy/EEG_Data_Raw/resting_2/'
+output_folder = '/mnt/raid/RU1/Raw_data/Ettore/Entropy/Healthy/Preprocessing_Output_2/'
 
 % Create output folder if it does not exist
 if ~exist(output_folder, 'dir')
@@ -14,7 +15,7 @@ if ~exist(output_folder, 'dir')
 end
 
 % Get file list of all the resting state recordings
-file_list = dir(fullfile(input_folder, '*cnt'))
+file_list = dir(fullfile(input_folder, '*.vhdr'))
 
 for i = 1:length(file_list)
 
@@ -22,7 +23,7 @@ for i = 1:length(file_list)
     file_name = file_list(i).name
 
     % Load EEG recording on EEGLAB
-    EEG = pop_loadcnt([input_folder file_name], 'dataformat', 'auto', 'memmapfile', '')
+    EEG = pop_loadbv([input_folder file_name], 'dataformat', 'auto', 'memmapfile', '')
     
     % Create full path for output files
     [~, baseName, ~] = fileparts(file_name)
